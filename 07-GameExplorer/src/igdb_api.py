0158893a4c1f6a5c7d5deb1a1fa8f491caaa990c
Cristiano Oliveira <cr.oliveira1989@gmail.com>
2026-08-18 20:17:03 -0300

    Adiciona consulta de detalhes e formatação de data pela IGDB

--- 07-GameExplorer/src/igdb_api.py
+++ 07-GameExplorer/src/igdb_api.py
@@ -1,8 +1,7 @@
 import requests
-
+from datetime import datetime
 
 URL_JOGOS = "https://api.igdb.com/v4/games"
-
 
 def buscar_jogos(token, client_id, nome_jogo):
     headers = {
@@ -12,7 +11,7 @@ def buscar_jogos(token, client_id, nome_jogo):
 
     body = f'''
         search "{nome_jogo}";
-        fields name;
+        fields id, name;
     '''
 
     response = requests.post(
@@ -22,6 +21,28 @@ def buscar_jogos(token, client_id, nome_jogo):
     )
     return response
 
+def buscar_detalhes_jogo(token, client_id, jogo_id):
+    headers = {
+        "Client-ID": client_id,
+        "Authorization": f"Bearer {token}"
+    }
+
+    body = f'''
+        where id = {jogo_id};
+        fields name, summary, first_release_date;
+    '''
+
+    response = requests.post(
+        URL_JOGOS,
+        headers=headers,
+        data=body
+    )
+    return response
+
+def formatar_data(timestamp):
+    data = datetime.fromtimestamp(timestamp)
+    return data.strftime("%d/%m/%Y")
+
 
 if __name__ == "__main__":
     from auth import obter_credenciais, obter_token
@@ -29,7 +50,8 @@ if __name__ == "__main__":
     client_id, client_secret = obter_credenciais()
     token = obter_token()
 
-    resposta = buscar_jogos(token, client_id, "Minecraft")
+    resposta = buscar_detalhes_jogo(token, client_id, 135400)
 
     print(resposta.status_code)
     print(resposta.text)
+    print(formatar_data(1482105600))
