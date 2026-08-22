59bbf76fce1ec48c9fa66e78d011afdad97dd246
Cristiano Oliveira <cr.oliveira1989@gmail.com>
2026-08-21 21:39:50 -0300

    Adiciona consulta e tratamento de plataformas

--- 07-GameExplorer/src/igdb_api.py
+++ 07-GameExplorer/src/igdb_api.py
@@ -3,6 +3,7 @@ from datetime import datetime
 
 URL_JOGOS = "https://api.igdb.com/v4/games"
 URL_GENEROS = "https://api.igdb.com/v4/genres"
+URL_PLATAFORMAS = "https://api.igdb.com/v4/platforms"
 
 def buscar_jogos(token, client_id, nome_jogo):
     headers = {
@@ -30,7 +31,7 @@ def buscar_detalhes_jogo(token, client_id, jogo_id):
 
     body = f'''
         where id = {jogo_id};
-        fields name, summary, first_release_date, rating, genres;
+        fields name, summary, first_release_date, rating, genres, platforms;
     '''
 
     response = requests.post(
@@ -65,6 +66,34 @@ def buscar_generos(token, client_id, ids_generos):
         nomes_generos.append(genero["name"])
     return nomes_generos
 
+def buscar_plataformas(token, client_id, ids_plataformas):
+    headers = {
+        "Client-ID": client_id,
+        "Authorization": f"Bearer {token}"
+    }
+
+    ids = ",".join(map(str, ids_plataformas))
+
+    body = f'''
+        where id = ({ids});
+        fields name;
+    '''
+
+    response = requests.post(
+        URL_PLATAFORMAS,
+        headers=headers,
+        data=body
+    )
+
+    plataformas = response.json()
+
+    nomes_plataformas = []
+
+    for plataforma in plataformas:
+        nomes_plataformas.append(plataforma["name"])
+
+    return nomes_plataformas
+
 def formatar_data(timestamp):
     data = datetime.fromtimestamp(timestamp)
     return data.strftime("%d/%m/%Y")
@@ -73,20 +102,34 @@ if __name__ == "__main__":
     from auth import obter_credenciais, obter_token
 
     client_id, client_secret = obter_credenciais()
+
     token = obter_token()
 
     resposta = buscar_detalhes_jogo(token, client_id, 135400)
+
     jogo = resposta.json()[0]
+
     timestamp = jogo["first_release_date"]
 
     data_lancamento = formatar_data(timestamp)
 
     ids_generos = jogo["genres"]
 
-nomes_generos = buscar_generos(
-    token,
-    client_id,
-    ids_generos
-)
-print(data_lancamento)
-print(nomes_generos)
+    nomes_generos = buscar_generos(
+        token,
+        client_id,
+        ids_generos
+    )
+
+    ids_plataformas = jogo["platforms"]
+
+    nomes_plataformas = buscar_plataformas(
+        token,
+        client_id,
+        ids_plataformas
+    )
+
+    print(data_lancamento)
+    print(nomes_generos)
+    print(nomes_plataformas)
+
