c8f319eee52d0a34be818061030fa6539ca4700b
Cristiano Oliveira <cr.oliveira1989@gmail.com>
2026-08-27 20:04:03 -0300

    Refatora fluxo principal e separa responsabilidades da IGDB

--- 07-GameExplorer/src/main.py
+++ 07-GameExplorer/src/main.py
@@ -1,5 +1,12 @@
 from auth import obter_credenciais, obter_token
-from twitch_api import buscar_jogos
+from igdb_api import (
+    buscar_jogos,
+    buscar_detalhes_jogo,
+    buscar_generos,
+    buscar_plataformas,
+    formatar_data,
+    exibir_jogo
+)
 
 # Obtém as credenciais da aplicação
 client_id, client_secret = obter_credenciais()
@@ -15,13 +22,54 @@ while True:
 
     resposta = buscar_jogos(token, client_id, nome_jogo)
 
-    if resposta is None:
-        print("Não foi possível buscar o jogo.")
+    resultados = resposta.json()
 
-    else:
-        if resposta["data"]:
-            jogo = resposta["data"][0]
-            print(jogo)
-        else:
-            print("Jogo não encontrado.")
- 
+    for indice, jogo in enumerate(resultados, start=1):
+        print(indice, "-", jogo["name"])
+
+    escolha = int(input("Escolha um jogo: "))
+
+    jogo_escolhido = resultados[escolha - 1]
+
+    jogo_id = jogo_escolhido["id"]
+
+    resposta = buscar_detalhes_jogo(token, client_id, jogo_id)
+
+    jogo = resposta.json()[0]
+
+    nome = jogo["name"]
+    resumo = jogo["summary"]
+
+    timestamp = jogo["first_release_date"]
+    data_lancamento = formatar_data(timestamp)
+
+    ids_generos = jogo["genres"]
+
+    nomes_generos = buscar_generos(
+        token,
+        client_id,
+        ids_generos
+    )
+
+    generos = ", ".join(nomes_generos)
+
+    ids_plataformas = jogo["platforms"]
+
+    nomes_plataformas = buscar_plataformas(
+        token,
+        client_id,
+        ids_plataformas
+    )
+
+    plataformas = ", ".join(nomes_plataformas)
+
+    rating = round(jogo["rating"], 2)
+
+    exibir_jogo(
+        nome,
+        data_lancamento,
+        generos,
+        plataformas,
+        resumo,
+        rating
+    )
