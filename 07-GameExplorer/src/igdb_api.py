bc98a54b80acac3f0f54fa186d76849eceafa197
Cristiano Oliveira <cr.oliveira1989@gmail.com>
2026-09-15 20:13:18 -0300

    Trata falhas de conexão nos detalhes do jogo

--- 07-GameExplorer/src/igdb_api.py
+++ 07-GameExplorer/src/igdb_api.py
@@ -49,16 +49,20 @@ def buscar_detalhes_jogo(token, client_id, jogo_id):
         fields name, summary, first_release_date, rating, genres, platforms;
     '''
 
-    response = requests.post(
-        URL_JOGOS,
-        headers=headers,
-        data=body
-    )
-
     try:
+        response = requests.post(
+            URL_JOGOS,
+            headers=headers,
+            data=body
+        )
+
         response.raise_for_status()
     except requests.exceptions.HTTPError:
         print("Erro na comunicação com a IGDB.")
+        return None
+
+    except requests.exceptions.RequestException:
+        print("Não foi possível conectar à IGDB.")
         return None
 
     return response
@@ -87,7 +91,7 @@ def buscar_nomes(token, client_id, url, ids):
         return None
 
     dados = response.json()
-    
+
     nomes = []
 
     for item in dados:
@@ -151,7 +155,7 @@ def preparar_jogo(token, client_id, jogo):
         generos = "Não disponível."
 
     ids_plataformas = jogo.get("platforms", [])
-    
+
     if ids_plataformas:
         nomes_plataformas = buscar_plataformas(
             token,
