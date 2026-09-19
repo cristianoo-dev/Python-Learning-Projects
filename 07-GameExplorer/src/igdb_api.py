a71a8e930f8490cba71b9f6ce579985469b55036
Cristiano Oliveira <cr.oliveira1989@gmail.com>
2026-09-19 10:34:58 -0300

    Refatora comunicação com a IGDB

--- 07-GameExplorer/src/igdb_api.py
+++ 07-GameExplorer/src/igdb_api.py
@@ -84,21 +84,13 @@ def buscar_nomes(token, client_id, url, ids):
         fields name;
     '''
 
-    try:
-        response = requests.post(
-            url,
-            headers=headers,
-            data=body
-        )
+    response = fazer_requisicao(
+        url,
+        headers,
+        body
+    )
 
-        response.raise_for_status()
-
-    except requests.exceptions.HTTPError:
-        print("Erro na comunicação com a IGDB.")
-        return None
-
-    except requests.exceptions.RequestException:
-        print("Não foi possível conectar à IGDB.")
+    if response is None:
         return None
 
     dados = response.json()
